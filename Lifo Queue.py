b3d0425c601d9eeb1a171edd91d723cf593fb27a
Ammad Yousaf <ammyyou112@gmail.com>
2023-02-17 17:56:11 +0500

    LIFO Queue Program

---
+++ Lifo Queue.py
@@ -0,0 +1,17 @@
+import queue
+
+queue = []
+
+#add items in queue
+queue.append(10)
+queue.append(20)
+queue.append(30)
+
+#print the queue
+print(queue)
+
+#remove the element
+remove = queue.pop()
+
+#print the result
+print('First Out from Queue',remove)
